41b6b94d139607f2d421c371f725bd84f938b9ae
Michael Masakayan <masakayan@chapman.edu>
2022-10-16 08:50:59 -0700

    changed home

---
+++ divvyup/src/pages/Upload copy.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import MyImage from './upload2.png';
+import './styles.css';
+import './upload.css'
+
+const UploadAndDisplayImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <div class="mainCountainer">
+     <input type="file" onChange={this.fileChangedHandler}>
+      <button onClick={this.uploadHandler}>Upload!</button>
+      state = { selectedFile: null }
+
+fileChangedHandler = event => {
+  this.setState({ selectedFile: event.target.files[0] })
+}
+uploadHandler = () => {
+  const formData = new FormData()
+  formData.append(
+    'myFile',
+    this.state.selectedFile,
+    this.state.selectedFile.name
+  )
+  axios.post('my-domain.com/file-upload', formData){
+    onUploadProgress: progressEvent => {
+      console.log(progressEvent.loaded / progressEvent.total)
+    }
+  })
+
+}
+
+uploadHandler = () => {
+  console.log(this.state.selectedFile)
+}
+    </div>
+  );
+};
+
+export default UploadAndDisplayImage;
