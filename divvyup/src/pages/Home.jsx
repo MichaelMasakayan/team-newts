5096c9add5490392fa94dc6b394fb2ed20d92d9d
Michael Masakayan <masakayan@chapman.edu>
2022-10-16 03:56:30 -0700

    changed color and home page

--- divvyup/src/pages/Home.jsx
+++ divvyup/src/pages/Home.jsx
@@ -9,53 +9,90 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
+import Slide from '@mui/material/Slide';
+import { motion } from "framer-motion";
 
 
-const icon = (
-  <Paper sx={{ m: 1 }} elevation={4}>
-    <Box component="svg" sx={{ width: 100, height: 100 }}>
-      <Box
-        component="polygon"
-        sx={{
-          fill: (theme) => theme.palette.common.white,
-          stroke: (theme) => theme.palette.divider,
-          strokeWidth: 1,
-        }}
-        points="0,100 50,00, 100,100"
-      />
-    </Box>
-  </Paper>
-);
 
-function SimpleGrow() {
-  const [checked, setChecked] = React.useState(false);
+// const icon = (
+//   <Paper sx={{ m: 1 }} elevation={4}>
+//     <Box component="svg" sx={{ width: 100, height: 100 }}>
+//       <Box
+//         component="polygon"
+//         sx={{
+//           fill: (theme) => theme.palette.common.white,
+//           stroke: (theme) => theme.palette.divider,
+//           strokeWidth: 1,
+//         }}
+//         points="0,100 50,00, 100,100"
+//       />
+//     </Box>
+//   </Paper>
+// );
 
-  const handleChange = () => {
-    setChecked((prev) => !prev);
-  };
+// function SimpleGrow() {
+//   const [checked, setChecked] = React.useState(false);
 
-  return (
-    <Box sx={{ height: 180 }}>
-      <FormControlLabel
-        control={<Switch checked={checked} onChange={handleChange} />}
-        label="Show"
-      />
-      <Box sx={{ display: 'flex' }}>
-        <Grow in={checked}>{icon}</Grow>
-        {/* Conditionally applies the timeout prop to change the entry speed. */}
-      </Box>
-    </Box>
-  );
-}
+//   const handleChange = () => {
+//     setChecked((prev) => !prev);
+//   };
 
+//   return (
+//     <Box sx={{ height: 180 }}>
+//       <FormControlLabel
+//         control={<Switch checked={checked} onChange={handleChange} />}
+//         label="Show"
+//       />
+//       <Box sx={{ display: 'flex' }}>
+//         <Grow in={checked}>{icon}</Grow>
+//         {/* Conditionally applies the timeout prop to change the entry speed. */}
+//       </Box>
+//     </Box>
+//   );
+// }
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function Home() {
   return(
     <div>
+             <motion.div
+      className="intro-text"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2.2 }}
+    >
+      <h1 class="container"> Splitting the Bill, Made Easy</h1>
+      <Grid container spacing={2}>
+  <Grid item xs={6} md={8}>
+    <Item>xs=6 md=8</Item>
+  </Grid>
+  <Grid item xs={6} md={4}>
+    <Item>xs=6 md=4</Item>
+  </Grid>
+  <Grid item xs={6} md={4}>
+    <Item>xs=6 md=4</Item>
+  </Grid>
+  <Grid item xs={6} md={8}>
+    <Item>xs=6 md=8</Item>
+  </Grid>
+</Grid>
+    </motion.div> 
+
+
       <Container maxWidth='xl'>
         <Box sx={{ flexGrow: 1, }}>
           <Grid container spacing={2}>
             <Grid item xs={8}>
-              <Typography
+            {/* <Slide direction="up" in timout mountOnEnter unmountOnExit>
+            <h1> Splitting the Bill, Made Easy</h1>
+  </Slide> */}
+      
+              {/* <Typography
                 variant="h2"
                 sx={{
                   mr: 2,
@@ -66,7 +103,7 @@ function Home() {
                 }}
               >
                 Splitting the Bill, Made Easy
-              </Typography>
+              </Typography> */}
             </Grid>
             <Grid item xs={4}>
               {/* PUT IMAGE HERE <Box component={img}></Box>*/}
@@ -104,7 +141,7 @@ function Home() {
           </Box>
         </Box>*/}
       </Container>
-      <SimpleGrow/>
+      {/* <SimpleGrow/> */}
     </div>
   );
 }
