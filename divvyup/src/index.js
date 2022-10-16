f2f1620f603949a8f54999e442d32f59ce7e023e
StevenAbouchedid <52302393+StevenAbouchedid@users.noreply.github.com>
2022-10-15 18:45:48 -0700

    Added Routing ie Pagination

---
+++ divvyup/src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ divvyup/src/pages/NoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoPage() {
+
+    return (
+        <div>
+            SOMETHING WENT WRONG WHOOOOOOP DE DOOOOOOO
+      </div>
+    );
+}
+
+export default NoPage;
