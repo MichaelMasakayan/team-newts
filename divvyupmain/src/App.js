4e9f830a6bddb8a27541bc1c560fb3d35f8cd0d1
Michael Masakayan <masakayan@chapman.edu>
2022-10-15 17:26:40 -0700

    react changes

--- divvyupmain/src/App.js
+++ divvyupmain/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="DivvyUp-item-list">
+      <table>
+        <thead>
+          {/* headings */}
+          <tr>
+            <th>Item</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* items */}
+          <tr>
+            <td> Burger</td>
+            <td> $10</td>
+          </tr>
+          </tbody>
+      </table>
       <h1>Divvy Up</h1>
     </div>
   );
