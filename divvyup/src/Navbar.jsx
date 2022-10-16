9ddec5650383ee624639a5eb50deb14bb7224691
StevenAbouchedid <52302393+StevenAbouchedid@users.noreply.github.com>
2022-10-15 19:50:46 -0700

    Navbar Changes

--- divvyup/src/Navbar.jsx
+++ divvyup/src/Navbar.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+// import React from "react";
+
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//         <li>
+//           <Link to="/contact">Contact</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+import * as React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            DivvyUp
+          </Typography>
+          <Button color="inherit">
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/about" style={{ textDecoration: 'none', color: 'white' }}>About</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/contacts" style={{ textDecoration: 'none', color: 'white' }}>Contacts</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
