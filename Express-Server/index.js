e5ca7c2a6e11629be3bfef12e4099ce70c814d78
StevenAbouchedid <52302393+StevenAbouchedid@users.noreply.github.com>
2022-10-16 00:33:51 -0700

    DataGrid Changes + some express server changes
    express server in test mode still, not fully connected with api yet

--- Express-Server/index.js
+++ Express-Server/index.js
@@ -1,34 +1,35 @@
 const express = require( 'express' )
 const app     = express()
-const port    = process.env.PORT || 3001
+const cors    = require('cors')
+const port    = process.env.PORT || 5000
 
-const Client = require('@veryfi/veryfi-sdk');
-const client_id = 'vrfuA9F2MsPoz2OvR4arAdcVJDDWrTHzC7L0F9R';
-const client_secret = 'K5I7xOSFI8cIp4hd57x376L911Y9YctHKeY0gGTAH48YKUz6jDuVDs8bWhHTWErGVl3LVEk9evS0jXEoOZwgG0JudNMZTc8dxHXeJGFyfWBg8vg740Uuev5YDtkoLO1M';
-const username = 'masakayan';
-const api_key = 'd3cb58908264ab2ee916fa292216ac9c';
+// const Client = require('@veryfi/veryfi-sdk');
+// const client_id = 'vrfuA9F2MsPoz2OvR4arAdcVJDDWrTHzC7L0F9R';
+// const client_secret = 'K5I7xOSFI8cIp4hd57x376L911Y9YctHKeY0gGTAH48YKUz6jDuVDs8bWhHTWErGVl3LVEk9evS0jXEoOZwgG0JudNMZTc8dxHXeJGFyfWBg8vg740Uuev5YDtkoLO1M';
+// const username = 'masakayan';
+// const api_key = 'd3cb58908264ab2ee916fa292216ac9c';
 
-const categories = ['Grocery', 'Utilities', 'Travel'];
-const file_path = "reciept.png";
+// const categories = ['Grocery', 'Utilities', 'Travel'];
+// const file_path = "reciept.png";
 
-const clientFunction = (url) => {
-    return new Promise((resolve, reject) => {
-        let veryfi_client = new Client(client_id, client_secret, username, api_key);
-        let response = veryfi_client.process_document_url(String(url));
-        resolve(response);
-    });
-}
+// const clientFunction = (url) => {
+//     return new Promise((resolve, reject) => {
+//         let veryfi_client = new Client(client_id, client_secret, username, api_key);
+//         let response = veryfi_client.process_document_url(String(url));
+//         resolve(response);
+//     });
+// }
 
-async function callingFunction(url){
-    return await(clientFunction(url));
-}
+// async function callingFunction(url){
+//     return await(clientFunction(url));
+// }
 
-function parseImageURL(url){
-    let urlArray = url.split('_');
-    let fileName = "https://i.ibb.co/" + urlArray[0] + "/" + urlArray[1] +".png";
-    console.log(fileName);
-    return fileName;
-}
+// function parseImageURL(url){
+//     let urlArray = url.split('_');
+//     let fileName = "https://i.ibb.co/" + urlArray[0] + "/" + urlArray[1] +".png";
+//     console.log(fileName);
+//     return fileName;
+// }
 
 function parseResponseJson(json){
 
@@ -49,20 +50,40 @@ function parseResponseJson(json){
     return response;
 }
 
+app.use(cors())
 
 app.get( '/' ,(req, res) => {
 res.type( 'text/plain' )
 res.send( 'Server Expresso ☕' )
 })
 
-app.get( '/:imgURL', ( req, res ) => {
-    res.type( 'application/json' )
-    let parsedImageURL = parseImageURL(req.params.imgURL);
-    callingFunction(parsedImageURL).then((response) => {
-        res.send(parseResponseJson(response));
-        console.log(parseResponseJson(response));
-    });
-})
+app.get( '/testJson' ,(req, res) => {
+    res.type('application/json')
+    console.log("JSON SENT ! ! ! ")
+    res.send(
+        {
+        subtotal: 32.82,
+        tax: 2.54,
+        total: 35.36,
+        products: [
+          { name: 'VINYL GLOVES', price: 11.72, quantity: 1 },
+          { name: 'AJAX DISHLIM', price: 2.96, quantity: 1 },
+          { name: 'ADVIL DUAL18', price: 3.98, quantity: 1 },
+          { name: 'MCC/SCH PARS', price: 2.44, quantity: 1 },
+          { name: 'VINYL GLOVES', price: 11.72, quantity: 1 }
+        ]
+      }
+    )
+});
+
+// app.get( '/:imgURL', ( req, res ) => {
+//     res.type( 'application/json' )
+//     let parsedImageURL = parseImageURL(req.params.imgURL);
+//     callingFunction(parsedImageURL).then((response) => {
+//         res.send(parseResponseJson(response));
+//         console.log(parseResponseJson(response));
+//     });
+// })
 
 app.use( ( req, res ) => {
 res.type( 'text/plain' )
